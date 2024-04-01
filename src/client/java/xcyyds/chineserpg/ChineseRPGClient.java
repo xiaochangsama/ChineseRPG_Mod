86c5b45531d8030f771b3f5151b89851bf92185e
xiaochangsama <103060641+xiaochangsama@users.noreply.github.com>
2024-04-01 10:05:48 +0800

    initialize

---
+++ src/client/java/xcyyds/chineserpg/ChineseRPGClient.java
@@ -0,0 +1,10 @@
+package xcyyds.chineserpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChineseRPGClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
