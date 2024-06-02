4b695f1a5738d4845eaeeb534d8683ae93fb7997
xiaochangsama <1354281282@qq.com>
2024-06-02 23:42:46 +0800

    Update PlayerEntityMixin.java

--- src/main/java/xcyyds/chineserpg/mixin/PlayerEntityMixin.java
+++ src/main/java/xcyyds/chineserpg/mixin/PlayerEntityMixin.java
@@ -1,56 +1,75 @@
 package xcyyds.chineserpg.mixin;
 
-import com.google.common.collect.ImmutableList;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xcyyds.chineserpg.event.PlayerTickCallback;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
-/*
- * 双重转型：
- * ((PlayerEntity)(object)this)为了调用目标类的方法，不建议使用这种方式
- *
- * 方法、字段来自目标类，可以使用@Shadow来获取
- *
- * 方法字段来自父类，可以直接让此类继承父类，然后调用父类方法（下面代码继承了爷爷类）
- */
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends Entity {//这里继承了爷爷类，因此可以使用爷爷类的方法
-    //因为要继承爷爷类
+public abstract class PlayerEntityMixin extends Entity {
+
+    @Shadow public abstract void sendMessage(Text message, boolean overlay);
+
+    private static final TrackedData<Float> INNER_POWER = DataTracker.registerData(PlayerEntityMixin.class, TrackedDataHandlerRegistry.FLOAT);
+    private static final TrackedData<Float> INNER_POWER_MAX = DataTracker.registerData(PlayerEntityMixin.class, TrackedDataHandlerRegistry.FLOAT);
+    private static final TrackedData<Integer> JUMP_COUNT = DataTracker.registerData(PlayerEntityMixin.class, TrackedDataHandlerRegistry.INTEGER);
+
+    private NbtCompound customData;
+
     public PlayerEntityMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    //代码执行前执行
+    @Inject(at = @At("HEAD"), method = "initDataTracker")
+    private void initDataTracker(CallbackInfo info) {
+        this.getDataTracker().startTracking(INNER_POWER, 0.0f);
+        this.getDataTracker().startTracking(INNER_POWER_MAX, 100.0f);
+        this.getDataTracker().startTracking(JUMP_COUNT, 0);
+    }
+
+    @Inject(at = @At("HEAD"), method = "writeCustomDataToNbt")
+    private void writeCustomDataToNbt(NbtCompound nbt, CallbackInfo info) {
+        nbt.putDouble("InnerPower", this.getDataTracker().get(INNER_POWER));
+        nbt.putDouble("InnerPowerMax", this.getDataTracker().get(INNER_POWER_MAX));
+        nbt.putInt("JumpCount", this.getDataTracker().get(JUMP_COUNT));
+        customData = nbt;
+    }
+
+    @Inject(at = @At("HEAD"), method = "readCustomDataFromNbt")
+    private void readCustomDataFromNbt(NbtCompound nbt, CallbackInfo info) {
+        this.getDataTracker().set(INNER_POWER, nbt.getFloat("InnerPower"));
+        this.getDataTracker().set(INNER_POWER_MAX, nbt.getFloat("InnerPowerMax"));
+        this.getDataTracker().set(JUMP_COUNT, nbt.getInt("JumpCount"));
+        customData = nbt;
+    }
+
     @Inject(at = @At("HEAD"), method = "tick")
-    private void tickHead(CallbackInfo info){
-//        //让玩家躺下，测试用;运行结果是，只在某些情况下才能触发，其他地方调用的此方法导致运行出错
-//        this.setPose(EntityPose.SLEEPING);
-    }
+    private void tick(CallbackInfo info) {
+        // 示例：每个tick增加自定义数据中的整数
+        Integer jumpCount = this.getDataTracker().get(JUMP_COUNT);
+        this.getDataTracker().set(JUMP_COUNT, jumpCount+1);
 
-    //代码返回前执行
-    @Inject(at = @At("RETURN"), method = "tick")
-    private void tickReturn(CallbackInfo info){
-    }
+        Float innerPower = this.getDataTracker().get(INNER_POWER);
+        this.getDataTracker().set(INNER_POWER, innerPower+1);
 
-//    //定义一个事件
-//    @Inject(at = @At(value = "INVOKE", target = ""), method = "tick")
-//    private void onPlayerTick(CallbackInfo info){
-//
-//            ActionResult result = PlayerTickCallback.EVENT.invoker().interact((PlayerEntity)(Object)this);
-//
-//            if(result == ActionResult.FAIL) {
-//                info.cancel();
-//            }
-//
-//    }
+        Float innerPowerMax = this.getDataTracker().get(INNER_POWER_MAX);
+        this.getDataTracker().set(INNER_POWER_MAX, innerPowerMax+1);
+
+        this.sendMessage(Text.of("InnerPower:"+this.getDataTracker().get(INNER_POWER)));
+        this.sendMessage(Text.of("InnerPowerMax:"+this.getDataTracker().get(INNER_POWER_MAX)));
+        this.sendMessage(Text.of("JumpCount:"+this.getDataTracker().get(JUMP_COUNT)));
+    }
 }
