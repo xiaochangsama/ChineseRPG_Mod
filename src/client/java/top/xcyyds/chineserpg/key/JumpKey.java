6e5e9689beaf334e9a2b94a4c0f21926509f41ad
xiaochangsama <1354281282@qq.com>
2024-06-06 01:52:40 +0800

    补丁

--- src/client/java/top/xcyyds/chineserpg/key/JumpKey.java
+++ src/client/java/top/xcyyds/chineserpg/key/JumpKey.java
@@ -6,19 +6,27 @@ import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 import top.xcyyds.chineserpg.network.ClientJumpKeySyncHandler;
 
+import java.util.Objects;
+
 public class JumpKey {
     private static boolean wasPressed = false;
+    private static boolean wasOnGround = false;
 
     public static void registryJumpKey() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             boolean isPressed = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), GLFW.GLFW_KEY_SPACE);
-            if (isPressed && !wasPressed) {
+
+            if (MinecraftClient.getInstance().player != null) {
+                boolean isOnGround = MinecraftClient.getInstance().player.isOnGround();
+
+            if ((isPressed && !wasPressed) && (isOnGround == wasOnGround ) ) {
                 if (MinecraftClient.getInstance().player != null && MinecraftClient.getInstance().getNetworkHandler() != null) {
-                    //空格被按下一次，发送一次数据包
-                    ClientJumpKeySyncHandler.sendJumpKeyStatus(true);
+                    ClientJumpKeySyncHandler.sendJumpKeyStatus(true, isOnGround);
                 }
             }
             wasPressed = isPressed;
+            wasOnGround = isOnGround;
+            }
         });
     }
 }
--- src/client/java/top/xcyyds/chineserpg/network/ClientJumpKeySyncHandler.java
+++ src/client/java/top/xcyyds/chineserpg/network/ClientJumpKeySyncHandler.java
@@ -1,7 +1,6 @@
 package top.xcyyds.chineserpg.network;
 
 import io.netty.buffer.Unpooled;
-
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.PacketByteBuf;
@@ -12,11 +11,12 @@ public class ClientJumpKeySyncHandler {
 
     public static final Identifier JUMP_KEY_SYNC = new Identifier(ChineseRPG.MOD_ID, "jump_key_sync");
 
-    //发送客户端键盘空格被按压的状态
-    public static void sendJumpKeyStatus(boolean isJumping) {
+    //发送客户端键盘空格被按压的状态和玩家是否在地面上
+    public static void sendJumpKeyStatus(boolean isJumping, boolean isOnGround) {
         if (MinecraftClient.getInstance().getNetworkHandler() != null) {
             PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
             buf.writeBoolean(isJumping);
+            buf.writeBoolean(isOnGround);
 
             ClientPlayNetworking.send(JUMP_KEY_SYNC, buf);
         }
