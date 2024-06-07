373065569bfd2205c02c225d21d9dee1627dd4d7
xiaochangsama <1354281282@qq.com>
2024-06-07 14:22:55 +0800

    fix

--- src/main/java/top/xcyyds/chineserpg/mixin/PlayerEntityMixin.java
+++ src/main/java/top/xcyyds/chineserpg/mixin/PlayerEntityMixin.java
@@ -11,9 +11,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.xcyyds.chineserpg.event.PlayerTravelCallback;
+import top.xcyyds.chineserpg.network.JumpKeySyncHandler;
 import top.xcyyds.chineserpg.player.data.IPlayerDataProvider;
 import top.xcyyds.chineserpg.player.data.PlayerData;
+import top.xcyyds.chineserpg.player.jump.PlayerJumpHandler;
 
+@SuppressWarnings("UnreachableCode")
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity implements IPlayerDataProvider {
     private final PlayerData playerData = new PlayerData();
@@ -33,5 +36,20 @@ public abstract class PlayerEntityMixin extends LivingEntity implements IPlayerD
     @Inject(method = "travel", at = @At("HEAD"))
     private void onTravelHead(Vec3d travelVector, CallbackInfo ci) {
         PlayerTravelCallback.EVENT.invoker().onTravel((PlayerEntity) (Object) this, travelVector);
+    }
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void playerTick(CallbackInfo ci){
+        if(JumpKeySyncHandler.wasPressed){
+            PlayerEntity player = (PlayerEntity) (Object) this;
+            PlayerData playerData1 =this.getPlayerData();
+            //实现对空中眺时对速度的操作
+                PlayerJumpHandler.toJump(player,  playerData1);
+            if (!this.getWorld().isClient) {
+
+                //System.out.println("JumpKeySyncHandler.wasPressed = false");
+            }
+
+            JumpKeySyncHandler.wasPressed = false;
+        }
     }
 }
