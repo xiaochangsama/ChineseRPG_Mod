bb74a5373dbf0737707602be677f56c36ed3b0f3
xiaochangsama <1354281282@qq.com>
2024-06-07 14:20:07 +0800

    fix

--- src/main/java/top/xcyyds/chineserpg/event/PlayerTravelEvent.java
+++ src/main/java/top/xcyyds/chineserpg/event/PlayerTravelEvent.java
@@ -2,6 +2,7 @@ package top.xcyyds.chineserpg.event;
 
 
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Vec3d;
 import top.xcyyds.chineserpg.player.data.IPlayerDataProvider;
 import top.xcyyds.chineserpg.player.data.PlayerData;
 
@@ -13,9 +14,17 @@ public class PlayerTravelEvent {
         PlayerTravelCallback.EVENT.register((player, travelVector) -> {
             // 处理事件逻辑
             PlayerData playerData = ((IPlayerDataProvider)player).getPlayerData();
+
+
+
             //我的playerData存储的速度信息目前会覆盖全部原版的速度更改
             if (playerData.isVelocityDirty()) {
-                player.setVelocity(player.getVelocity());
+                Vec3d vec3d = playerData.getPlayerVelocity();
+
+                player.setVelocity(vec3d);
+                player.velocityDirty = true;
+
+
                 playerData.setVelocityDirty(false);
             }
             return ActionResult.PASS;
