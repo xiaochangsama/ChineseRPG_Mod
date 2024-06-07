42541bce6773803fd9a8b8f5f443826019da0318
xiaochangsama <1354281282@qq.com>
2024-06-07 14:23:41 +0800

    fix

--- src/main/java/top/xcyyds/chineserpg/network/JumpKeySyncHandler.java
+++ src/main/java/top/xcyyds/chineserpg/network/JumpKeySyncHandler.java
@@ -1,8 +1,12 @@
 package top.xcyyds.chineserpg.network;
 
+
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.util.Identifier;
 import top.xcyyds.chineserpg.ChineseRPG;
+import top.xcyyds.chineserpg.player.data.IPlayerDataProvider;
+import top.xcyyds.chineserpg.player.data.PlayerData;
+
 
 
 /**
@@ -11,6 +15,7 @@ import top.xcyyds.chineserpg.ChineseRPG;
 public class JumpKeySyncHandler {
 
     public static final Identifier JUMP_KEY_SYNC = new Identifier(ChineseRPG.MOD_ID, "jump_key_sync");
+    public static boolean wasPressed = false;
 
     public static void register() {
         ServerPlayNetworking.registerGlobalReceiver(JUMP_KEY_SYNC, (server, player, handler, buf, responseSender) -> {
@@ -18,8 +23,11 @@ public class JumpKeySyncHandler {
 
             server.execute(() -> {
                 if (player != null) {
-
-
+                    PlayerData playerData = ((IPlayerDataProvider)player).getPlayerData();
+//                    //实现对空中眺时对速度的操作
+//                    PlayerJumpHandler.toJump(player,playerData);
+//                    sendJumpKeyStatus(player);
+                    wasPressed = true;
                 }
             });
         });
