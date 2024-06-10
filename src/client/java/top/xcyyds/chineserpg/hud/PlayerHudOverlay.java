7189433722fc9c8e00af4820e1e3e3c8aab2fa05
fanghongyang <347754565@qq.com>
2024-06-10 23:24:24 +0800

    内力值hud界面更改

--- src/client/java/top/xcyyds/chineserpg/hud/PlayerHudOverlay.java
+++ src/client/java/top/xcyyds/chineserpg/hud/PlayerHudOverlay.java
@@ -25,24 +25,50 @@ public class PlayerHudOverlay implements HudRenderCallback {
     private void renderInnerPowerBar(DrawContext drawContext, PlayerData playerData) {
         int x = 10;
         int y = 10;
-        int width = 100;
+        int width = 101;
         int height = 10;
+        int segmentCount = 34; // 将内力条分成34份
+        int segmentSpacing = 1; // 每个格子之间的间隔
+        int segmentWidth = (width - (segmentSpacing * (segmentCount - 1))) / segmentCount; // 每个格子的宽度
 
         int maxInnerPower = (int) playerData.getInnerPowerMax();
         int currentInnerPower = (int) playerData.getInnerPower();
 
         // 背景条
         drawContext.fill(x, y, x + width, y + height, 0xFF555555);
-        // 前景条
+
+        // 前景条和颜色渐变
         if (maxInnerPower > 0) {
-            int barWidth = (int) ((currentInnerPower / (float) maxInnerPower) * width);
-            drawContext.fill(x, y, x + barWidth, y + height, 0xFF00FF00);
+            int fullSegments = (currentInnerPower * segmentCount) / maxInnerPower;
+            for (int i = 0; i < fullSegments; i++) {
+                int color = interpolateColor(0xFF00FF00, 0xFF003300, i / (float) segmentCount);
+                drawContext.fill(x + i * (segmentWidth + segmentSpacing), y, x + i * (segmentWidth + segmentSpacing) + segmentWidth, y + height, color);
+            }
         }
 
         // 渲染内力值文本
-        String innerPowerText = "内力: " + currentInnerPower + "/" + maxInnerPower;
+        String innerPowerText = currentInnerPower + "/" + maxInnerPower;
         MinecraftClient client = MinecraftClient.getInstance();
-        drawContext.drawTextWithShadow(client.textRenderer, innerPowerText, x, y + height + 2, 0xFFFFFF);
+        drawContext.drawTextWithShadow(client.textRenderer, innerPowerText, x + width / 2 - client.textRenderer.getWidth(innerPowerText) / 2, y + 1, 0xFFFFFF);
+    }
+
+    private int interpolateColor(int startColor, int endColor, float ratio) {
+        int startA = (startColor >> 24) & 0xFF;
+        int startR = (startColor >> 16) & 0xFF;
+        int startG = (startColor >> 8) & 0xFF;
+        int startB = startColor & 0xFF;
+
+        int endA = (endColor >> 24) & 0xFF;
+        int endR = (endColor >> 16) & 0xFF;
+        int endG = (endColor >> 8) & 0xFF;
+        int endB = endColor & 0xFF;
+
+        int a = (int) (startA + ratio * (endA - startA));
+        int r = (int) (startR + ratio * (endR - startR));
+        int g = (int) (startG + ratio * (endG - startG));
+        int b = (int) (startB + ratio * (endB - startB));
+
+        return (a << 24) | (r << 16) | (g << 8) | b;
     }
 
     private void renderMartialArtName(DrawContext drawContext, PlayerData playerData) {
